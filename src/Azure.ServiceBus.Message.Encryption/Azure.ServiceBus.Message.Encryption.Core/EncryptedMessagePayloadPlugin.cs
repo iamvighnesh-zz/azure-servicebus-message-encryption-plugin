00fde849588eae3f1e2d2cc7fee0adfcb94468cf
Vighneshwar Madas <vighnesh.madas@gmail.com>
2020-01-17 14:05:15 +0000

    Add a cryptography provider interface

--- src/Azure.ServiceBus.Message.Encryption/Azure.ServiceBus.Message.Encryption.Core/EncryptedMessagePayloadPlugin.cs
+++ src/Azure.ServiceBus.Message.Encryption/Azure.ServiceBus.Message.Encryption.Core/EncryptedMessagePayloadPlugin.cs
@@ -1,15 +1,32 @@
-﻿using Microsoft.Azure.ServiceBus.Core;
+﻿using Azure.ServiceBus.Message.Encryption.Core.Providers;
+using Microsoft.Azure.ServiceBus.Core;
 using System.Threading.Tasks;
 
 namespace Azure.ServiceBus.Message.Encryption.Core
 {
     public class EncryptedMessagePayloadPlugin : ServiceBusPlugin
     {
-        public override string Name => nameof(EncryptedMessagePayloadPlugin);
+        private readonly ICryptographyProvider cryptographyProvider;
+
+        internal EncryptedMessagePayloadPlugin(ICryptographyProvider cryptographyProvider)
+        {
+            this.cryptographyProvider = cryptographyProvider;
+        }
+
+        public override string Name => typeof(EncryptedMessagePayloadPlugin).FullName;
 
         public override async Task<Microsoft.Azure.ServiceBus.Message> BeforeMessageSend(Microsoft.Azure.ServiceBus.Message message)
         {
+            message.Body = await cryptographyProvider.Encrypt(message.Body);
+
             return await base.BeforeMessageSend(message);
+        }
+
+        public override async Task<Microsoft.Azure.ServiceBus.Message> AfterMessageReceive(Microsoft.Azure.ServiceBus.Message message)
+        {
+            message.Body = await cryptographyProvider.Decrypt(message.Body);
+
+            return await base.AfterMessageReceive(message);
         }
     }
 }
---
+++ src/Azure.ServiceBus.Message.Encryption/Azure.ServiceBus.Message.Encryption.Core/Providers/ICryptographyProvider.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Azure.ServiceBus.Message.Encryption.Core.Providers
+{
+    public interface ICryptographyProvider
+    {
+        Task<byte[]> Encrypt(byte[] input);
+
+        Task<byte[]> Decrypt(byte[] input);
+    }
+}
