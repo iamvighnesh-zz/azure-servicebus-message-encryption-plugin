d26ec31902911569e9ae1b9b5f628b4414e8d580
Vighneshwar Madas <vighnesh.madas@gmail.com>
2020-01-17 13:54:44 +0000

    Add new plugin class for encryption

---
+++ src/Azure.ServiceBus.Message.Encryption/Azure.ServiceBus.Message.Encryption.Core/EncryptedMessagePayloadPlugin.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.ServiceBus.Core;
+using System.Threading.Tasks;
+
+namespace Azure.ServiceBus.Message.Encryption.Core
+{
+    public class EncryptedMessagePayloadPlugin : ServiceBusPlugin
+    {
+        public override string Name => nameof(EncryptedMessagePayloadPlugin);
+
+        public override async Task<Microsoft.Azure.ServiceBus.Message> BeforeMessageSend(Microsoft.Azure.ServiceBus.Message message)
+        {
+            return await base.BeforeMessageSend(message);
+        }
+    }
+}
