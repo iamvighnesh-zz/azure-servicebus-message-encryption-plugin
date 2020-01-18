1d827360ff6c3dff2515e49add8a6cb97fa65e11
Vighneshwar Madas <vighnesh.madas@gmail.com>
2020-01-18 06:12:05 +0000

    Add extensions to register the plugin into the sender or receiver

---
+++ src/Azure.ServiceBus.MessageEncryption/EncryptedMessagePayloadPluginExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Azure.ServiceBus.MessageEncryption.Providers;
+
+namespace Azure.ServiceBus.MessageEncryption
+{
+    public static class EncryptedMessagePayloadPluginExtensions
+    {
+        public static void RegisterMessageEncryptionPlugin(this Microsoft.Azure.ServiceBus.IClientEntity clientEntity, string cryptoKey)
+        {
+            ICryptographyProvider provider = new TripleDESCryptographyProvider(cryptoKey);
+
+            clientEntity.RegisterPlugin(new EncryptedMessagePayloadPlugin(provider));
+        }
+    }
+}
