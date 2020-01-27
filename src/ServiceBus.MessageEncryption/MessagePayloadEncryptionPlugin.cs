86282408ab308a79449b31b216d09b1d522a635d
Vighneshwar Madas <vighnesh.madas@gmail.com>
2020-01-27 23:56:37 +0000

    Enable Rijndael Managed encryption

--- src/ServiceBus.MessageEncryption/MessagePayloadEncryptionPlugin.cs
+++ src/ServiceBus.MessageEncryption/MessagePayloadEncryptionPlugin.cs
@@ -1,29 +1,34 @@
 ﻿using Microsoft.Azure.ServiceBus.Core;
 using ServiceBus.MessageEncryption.Providers;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace ServiceBus.MessageEncryption
 {
-    public class EncryptedMessagePayloadPlugin : ServiceBusPlugin
+    public class MessagePayloadEncryptionPlugin : ServiceBusPlugin
     {
         private readonly ICryptographyProvider cryptographyProvider;
 
-        public EncryptedMessagePayloadPlugin(ICryptographyProvider cryptographyProvider)
+        public MessagePayloadEncryptionPlugin(ICryptographyProvider cryptographyProvider)
         {
             this.cryptographyProvider = cryptographyProvider;
         }
 
-        public override string Name => typeof(EncryptedMessagePayloadPlugin).FullName;
+        public override string Name => typeof(MessagePayloadEncryptionPlugin).FullName;
 
         public override async Task<Microsoft.Azure.ServiceBus.Message> BeforeMessageSend(Microsoft.Azure.ServiceBus.Message message)
         {
-            message.Body = await cryptographyProvider.Encrypt(message.Body);
+            message.Body = await cryptographyProvider.Encrypt((byte[])message.Body.Clone());
+
+            var encryptedBody = Encoding.UTF8.GetString(message.Body);
 
             return await base.BeforeMessageSend(message);
         }
 
         public override async Task<Microsoft.Azure.ServiceBus.Message> AfterMessageReceive(Microsoft.Azure.ServiceBus.Message message)
         {
+            var encryptedBody = Encoding.UTF8.GetString(message.Body);
+
             message.Body = await cryptographyProvider.Decrypt(message.Body);
 
             return await base.AfterMessageReceive(message);
---
+++ src/ServiceBus.MessageEncryption/Providers/RijndaelManagedCryptographyProvider.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBus.MessageEncryption.Providers
+{
+    public class RijndaelManagedCryptographyProvider : ICryptographyProvider
+    {
+        private readonly byte[] cryptoKeyBytes;
+
+        private readonly byte[] initVectorKeyBytes;
+
+        public RijndaelManagedCryptographyProvider(string cryptoKey, string initVerctorKey)
+        {
+            cryptoKeyBytes = CreateCryptographyKey(cryptoKey);
+
+            initVectorKeyBytes = Encoding.UTF8.GetBytes(initVerctorKey);
+        }
+
+        public async Task<byte[]> Decrypt(byte[] input)
+        {
+            var decryptor = CreateDecryptor();
+
+            var inputBytes = Convert.FromBase64String(Convert.ToBase64String(input));
+
+            byte[] plainTextBytes = new byte[inputBytes.Length];
+
+            using MemoryStream memoryStream = new MemoryStream(inputBytes);
+
+            using CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+
+            await cryptoStream.ReadAsync(plainTextBytes, 0, plainTextBytes.Length);
+
+            return plainTextBytes;
+        }
+
+        public async Task<byte[]> Encrypt(byte[] input)
+        {
+            var encryptor = CreateEncryptor();
+
+            using MemoryStream memoryStream = new MemoryStream();
+            
+            using CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            
+            await cryptoStream.WriteAsync(input, 0, input.Length);
+            
+            cryptoStream.FlushFinalBlock();
+            
+            return memoryStream.ToArray();
+        }
+
+        private ICryptoTransform CreateEncryptor()
+        {
+            var password = new PasswordDeriveBytes(cryptoKeyBytes, null);
+            
+            var keyBytes = password.GetBytes(32);
+
+            return GetAlgorithm().CreateEncryptor(keyBytes, initVectorKeyBytes);
+        }
+
+        private ICryptoTransform CreateDecryptor()
+        {
+            var password = new PasswordDeriveBytes(cryptoKeyBytes, null);
+
+            var keyBytes = password.GetBytes(32);
+
+            return GetAlgorithm().CreateDecryptor(keyBytes, initVectorKeyBytes);
+        }
+
+        private SymmetricAlgorithm GetAlgorithm()
+        {
+            return new RijndaelManaged { Mode = CipherMode.CBC };
+        }
+
+        private byte[] CreateCryptographyKey(string cryptoKey)
+        {
+            var base64String = Convert.ToBase64String(Encoding.Unicode.GetBytes(cryptoKey));
+
+            return Encoding.UTF8.GetBytes(base64String);
+        }
+    }
+}
